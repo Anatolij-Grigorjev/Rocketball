0e52ea98926adedac13f06d8f1668134a6b52eae
Anatolij Grigorjev <anatolij@mediapark.lt>
2015-09-29 09:19:09 +0300

    request and response printing compactification

--- grails-app/conf/lt/mediapark/rocketball/BasicFilters.groovy
+++ grails-app/conf/lt/mediapark/rocketball/BasicFilters.groovy
@@ -5,19 +5,17 @@ class BasicFilters {
     def filters = {
         printStuff(controller: '*', action: '*') {
             before = {
-                log.debug("REQUEST:")
-                log.debug("------------------------------------------------------------")
-                log.debug("DESTINATION: ${request.requestURL.append(request.queryString ?: '')}")
-                log.debug("HEADERS: ${request.getHeaderNames().collect { it + "=" + request.getHeader(it) }}")
-                if (request.JSON) {
-                    log.debug("JSON: ${request.JSON}")
-                }
+                log.debug("REQUEST:\n"
+                        + "------------------------------------------------------------\n"
+                        + "DESTINATION: ${request.requestURL.append(request.queryString ?: '')}\n"
+                        + "HEADERS: ${request.getHeaderNames().collect { it + "=" + request.getHeader(it) }}\n"
+                + "${request.JSON? "JSON: ${request.JSON}" : ""}")
             }
             after = {
-                log.debug("RESPONSE:")
-                log.debug("------------------------------------------------------------")
-                log.debug("Status code: ${response.status}")
-                log.debug("HEADERS: ${response.getHeaderNames().collect { it + "=" + response.getHeader(it) }}")
+                log.debug("RESPONSE:\n" +
+                        "------------------------------------------------------------\n" +
+                        "Status code: ${response.status}\n" +
+                        "HEADERS: ${response.getHeaderNames().collect { it + "=" + response.getHeader(it) }}")
             }
         }
     }
